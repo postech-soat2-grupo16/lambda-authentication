437ae5576cf82889bd7ad4a0a85d204bac38cdc3
Pedro Haramoto <pedroharamoto@gmail.com>
2023-11-05 01:40:04 -0300

    remove var

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,12 +3,6 @@ variable "aws_region" {
   default = "us-east-1"
 }
 
-variable "s3_name" {
-  description = "S3 Name"
-  type = string
-  default = "terraform-state-soat"
-}
-
 variable "lambda_execution_role" {
   description = "Execution Role Lambda"
   type = string
